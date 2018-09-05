f412703aada84466d32bf4f222aa2d7c71f55f87
Admin <admin@mac-admin.local>
2018-09-05 15:12:00 +0300

    3 - loading avatar image

--- GitHubHelper/ViewController.swift
+++ GitHubHelper/ViewController.swift
@@ -50,6 +50,18 @@ class ViewController: UIViewController {
     func start()  {
         print("did load")
         userIDTextField.isHidden = true
+        
+        let url = URL(string: "https://avatars1.githubusercontent.com/u/42915692?v=4")
+        
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            DispatchQueue.main.async {
+              self.avatar.image = UIImage(data: data!)
+            }
+        }
+        
+        
+        
     }
     
     //MARK:- get data from site
